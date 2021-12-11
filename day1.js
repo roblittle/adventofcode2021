28277f2328b3115ea5dab8e2dee0fc1359c04e11
Rob Little <rob@littlelogic.ca>
2021-12-10 16:06:52 -0800

    var name

--- day1.js
+++ day1.js
@@ -20,17 +20,17 @@ const sonarSweepSingleVal = async(args) => {
 const sonarSweepSlidingTripples = async(args) => {
     let goingDeeper = 0;
     let left = 1;                   // INTENTIONALLY, so in 'middle' of left triplet
-    let leftSide = 2;               // leftSlide starts +1 to left
+    let leftSlide = 2;               // leftSlide starts +1 to left
     let right = args.length-2;      // INTENTIONALLY, so in 'middle' of right triplet
     let rightSlide = args.length-3; // rightSlide starts -1 to right
-    while(left < right && leftSide < rightSlide){
-        if(( args[left] + args[left-1] + args[left+1]) < (args[leftSide] + args[leftSide-1] + args[leftSide+1])) goingDeeper++;
+    while(left < right && leftSlide < rightSlide){
+        if(( args[left] + args[left-1] + args[left+1]) < (args[leftSlide] + args[leftSlide-1] + args[leftSlide+1])) goingDeeper++;
         if(( args[right] + args[right-1] + args[right+1]) > ( args[rightSlide] + args[rightSlide-1] + args[rightSlide+1] )) goingDeeper++;
-        left++, leftSide++;
+        left++, leftSlide++;
         right--, rightSlide--;
     };
     // IMPORTANT, when they are about to cross... do not double count, but do not miss it either. 1x needed!
-    if(( args[left] + args[left-1] + args[left+1]) < (args[leftSide] + args[leftSide-1] + args[leftSide+1])) goingDeeper++;
+    if(( args[left] + args[left-1] + args[left+1]) < (args[leftSlide] + args[leftSlide-1] + args[leftSlide+1])) goingDeeper++;
     console.log(goingDeeper);
     return goingDeeper;
 };
